831781c67200eca489270eda80449ac649ea2167
JulesGilli <60229102+JulesGilli@users.noreply.github.com>
2025-04-06 21:12:08 +0200

    fix: config

--- Speed-typo/vite.config.ts
+++ Speed-typo/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Speed-Typo/', // <<< ajoute cette ligne avec ton nom de repo exact
+  base: '/SpeedType/', // <<< ajoute cette ligne avec ton nom de repo exact
   plugins: [react()],
 })
